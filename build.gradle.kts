bf322dc18d7a3f4a1a68e07ac7b7b886209ca5e8
Adam Spofford <pieflavor.mc@gmail.com>
2019-06-18 19:01:40 -0700

    Split these up before it becomes a headache. https://github.com/pie-flavor/KludgeGradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.3.31"
     kotlin("kapt") version "1.3.31"
-    `java-gradle-plugin`
 }
 
 group = "flavor.pie"
@@ -13,18 +12,8 @@ repositories {
     mavenCentral()
 }
 
-gradlePlugin {
-    plugins {
-        create("kludge") {
-            id = "flavor.pie.kludge"
-            implementationClass = "flavor.pie.kludgec.KludgeGradlePlugin"
-        }
-    }
-}
-
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    implementation(kotlin("gradle-plugin-api"))
     val autoService = create("com.google.auto.service:auto-service:1.0-rc4")
     compileOnly(autoService)
     kapt(autoService)
