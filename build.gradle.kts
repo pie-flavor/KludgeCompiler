83ae58b3e8b7c2dace801a5e677f432105f813cb
Adam Spofford <pieflavor.mc@gmail.com>
2019-08-08 11:51:56 -0700

    Add publishing information

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
+import org.gradle.jvm.tasks.Jar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.3.31"
     kotlin("kapt") version "1.3.31"
     maven
+    `maven-publish`
 }
 
 group = "flavor.pie"
@@ -23,4 +25,53 @@ dependencies {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.register<Jar>("sourcesJar") {
+    from(sourceSets["main"].allSource)
+    classifier = "sources"
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("plugin") {
+            from(components["kotlin"])
+            artifact(tasks["sourcesJar"])
+            pom {
+                name.set("KludgeCompiler")
+                description.set("The compiler plugin companion to Kludge.")
+                url.set("https://github.com/pie-flavor/KludgeCompiler")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://github.com/pie-flavor/KludgeCompiler/blob/master/LICENSE")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("pie_flavor")
+                        name.set("Adam Spofford")
+                        email.set("aspofford.as@gmail.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/pie-flavor/KludgeCompiler.git")
+                    developerConnection.set("scm:git:ssh://github.com/pie-flavor/KludgeCompiler.git")
+                    url.set("https://github.com/pie-flavor/KludgeCompiler/")
+                }
+            }
+        }
+        repositories {
+            maven {
+                val spongePublishingUri: String by project
+                val spongePublishingUsername: String by project
+                val spongePublishingPassword: String by project
+                url = uri(spongePublishingUri)
+                credentials {
+                    username = spongePublishingUsername
+                    password = spongePublishingPassword
+                }
+            }
+        }
+    }
 }
